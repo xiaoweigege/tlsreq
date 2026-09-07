e1a62db0c191ceac68bbd41996174e7a04605b7c
ww <240942649@qq.com>
2026-09-08 00:00:27 +0800

    chore: release 0.1.3
    Ship the English README (and Chinese translation) to PyPI.

--- src/tlsreq/__init__.py
+++ src/tlsreq/__init__.py
@@ -3,7 +3,7 @@ from .response import Response
 from .session import AsyncSession, Session
 from .utls_release import UTLS_RELEASE, UTLS_RELEASE_PAGE
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = [
     "AsyncSession",
     "Session",
